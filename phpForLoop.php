931fc847532a580cfab1bfe7ad1cbfd1fd75607a
unknown <b00138240@mytudublin.ie>
2023-10-04 09:43:26 +0100

    count till 20

--- phpForLoop.php
+++ phpForLoop.php
@@ -1,9 +1,9 @@
 <?php
-#phpForLoop - put the name of the file/program here - notes the hashtag indicates a comment in PHP
-#put your name and the date here
+#phpForLoop - week3lab-FaisalAsghar07-1 - notes the hashtag indicates a comment in PHP
+#Faisal Asghar 04/10/2023
 #put a descripition of what the code does here - 
-#this program uses a loop to count up to 10 and puts the value of the loop counter out to the screen on each iteration
-for ($i=1;$i<=10;$i++) {
+#this program uses a loop to count up to 20 and puts the value of the loop counter out to the screen on each iteration
+for ($i=1;$i<=20;$i++) {
     echo "$i, ";
 }
 
