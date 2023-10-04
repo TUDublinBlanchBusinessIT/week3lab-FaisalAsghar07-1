b688d047a451c4099be7aabe37af759c62571f8c
unknown <b00138240@mytudublin.ie>
2023-10-04 10:17:55 +0100

    break every five number

---
+++ loopInFives.php
@@ -0,0 +1,15 @@
+<?php
+#phpForLoop - week3lab-FaisalAsghar07-1 - notes the hashtag indicates a comment in PHP
+#Faisal Asghar 04/10/2023
+#put a descripition of what the code does here - 
+#this program uses a loop to count up to 20 and puts the value of the loop counter out to the screen on each iteration
+for ($i=1;$i<=20;$i++) {
+    echo "$i, ";
+    if ($i%5==0) 
+    {
+        Echo "<br>";
+
+    }
+}
+
+?>
