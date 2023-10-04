d53300475dfa0d6f554d03d9ff032e94bba01d17
unknown <b00138240@mytudublin.ie>
2023-10-04 10:50:02 +0100

    stage 3

---
+++ nestedForLoop.php
@@ -0,0 +1,13 @@
+<?php
+echo "<TABLE border='1'>";
+for ($i=1;$i<=20;$i++) {
+
+    echo "<TR>";
+    for ($j=1; $j<=15; $j++) {
+    echo "<TD>row$j, col$i</TD>";
+
+    }
+    echo "</TR>";
+    }
+echo"</TABLE>";
+?>
